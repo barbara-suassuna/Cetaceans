82c06851bd6fd7154cd231afe4e2713451a29899
barbara-suassuna <barbarassuassuna@hotmail.com>
2025-10-14 20:17:27 -0600

    Update Updated_collecting_code.R

--- Updated_collecting_code.R
+++ Updated_collecting_code.R
@@ -174,3 +174,42 @@ write(mc_sequence, file="mc_gene.fasta")
 
 #Save TABLE in a file
 write.csv(whale, "mc_gene_seq_info.csv")
+
+
+#5 - MCPH1 GENE 
+#Create a Table
+whale<-matrix(,nrow=(length(parenttaxa$`180403`$taxonname)), ncol=4)
+colnames(whale)<-c("SpeciesName", "AccNum", "SeqName", "SeqLen")
+mcph_sequence<-character()
+
+for(i in 20: length(unique(parenttaxa$`180403`$taxonname))){
+  print(i)
+  whale[i,1]<-parenttaxa$`180403`$taxonname[i]
+  seqout<-entrez_search(db="nuccore", term=paste(parenttaxa$`180403`$taxonname[i], "[ORGN] AND 1:4500[SLEN] AND 
+                                                 MCPH1[ALL] AND biomol_genomic[PROP]",
+                                                 sep=""), retmax=1)
+  if(length(seqout$ids)<1){
+    whale[i,2]<-NA
+    whale[i,3]<-NA
+    whale[i,4]<-NA
+  }
+  
+  else{
+    seqout1<-entrez_summary(db="nuccore", id=seqout$ids)
+    whale[i,2]<-seqout1$accessionversion
+    whale[i,3]<-seqout1$title
+    whale[i,4]<-seqout1$slen
+    
+    seqout<-entrez_fetch(db="nuccore", id=seqout1$accessionversion, rettype="fasta")
+    seqout<-sub(">([^\n]*)", paste0(">", seqout1$organism), seqout)
+    seqout<-gsub(" ", "_", seqout)
+    mcph_sequence<-c(mcph_sequence, seqout)
+  }
+}
+
+#Save FASTA file
+write(mcph_sequence, file="mcph_gene.fasta")
+
+#Save TABLE in a file
+write.csv(whale, "mcph_gene_seq_info.csv")
+
